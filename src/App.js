e07fa844c3d9013224f1a6193de1fc1b221ece5c
Jimoo An <jmdh1004@gmail.com>
2023-11-06 19:11:21 +0900

    :sparkles: Todo List 기능 추가

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { List, Paper } from '@mui/material';
+import './App.css';
+import Todo from "./Todo";
+import { useState } from 'react';
+
+function App() {
+
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true
+    },
+  ]);
+
+  let todoItems = items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {items.map((item) => 
+            <Todo item={item} key={item.id} />
+          )}
+        </List>
+      </Paper>
+    );
+
+  return (
+    <div className="App">
+      {todoItems}
+    </div>
+  );
+}
+
+export default App;
