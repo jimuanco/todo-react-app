598d4f3d9954c2d3c22f5f72c088df0b80dc5df7
Jimoo An <jmdh1004@gmail.com>
2023-11-08 19:14:14 +0900

    :sparkles: Login page 추가

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,67 @@
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { API_BASE_URL } from "./api-config";
+import axios from "axios";
+
 const Login = () => {
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const username = data.get("username");
+    const password = data.get("password");
+    siginin({ username: username, password: password });
+  }
+
+  const siginin = (userDTO) => {
+    axios.post(`${API_BASE_URL}/auth/signin`, userDTO)
+      .then((response)=>{
+        response.data.token && (window.location.href = "/");
+      });
+  }
+
   return (
-    <p>로그인 페이지</p>
+    <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+        </Grid>
+      </Grid>
+      <form noValidate onSubmit={handleSubmit}>
+        {" "}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="아이디"
+              name="username"
+              autoComplete="username"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              type="password"
+              id="password"
+              label="패스워드"
+              name="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              로그인
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
   )
 }
 
